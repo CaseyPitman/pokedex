032bebb0b4bea588455264bbe76ced897e55340b
Casey The Great <casey@caseypitman.com>
2020-02-17 18:10:59 -0600

    added images

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 //Array containing Pokemon info
 
 const pokemonArr = [
-    {number:"001", name: "Bulbasaur", type: "Grass & Poison", ability:"Overgrow & Chlorophyll", image: }
+    {number:"001", name: "Bulbasaur", type: "Grass & Poison", ability:"Overgrow | Chlorophyll", image:"img/001Bulbasaur.png"},
+    {number:"002", name: "Ivysaur", type: "Grass & Poison", ability:"Overgrow | Chlorophyll", image:"img/002Ivysaur.png"},
+
+
 ];
